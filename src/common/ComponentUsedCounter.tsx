b409c283aee1dcd14c12c3f31012af0f2b900f7d
Im Moungsung <mslim1210@gmail.com>
2023-11-27 19:41:57 -0500

    for server

--- src/common/ComponentUsedCounter.tsx
+++ src/common/ComponentUsedCounter.tsx
@@ -6,6 +6,9 @@ export function ComponentUsedCounter(count: number,componentName: string) {
     //create보다는 update를 쓸 수 있으면 그러는 편이낫겠고 (db사용한다면)
     //아닐경우 그냥 어떻게든 이 값을 서버에 저장하는것으로 만족해도 좋겠다. ex) addButton 1 -> addButton 2 이런식으로..
     console.log(componentName,':', count)
+    
+  //이거를 서버에 올려서 a랑 분간을 해줘야함. 그래야 ab테스팅을 서로 분간해줄수잇음. 
+    const version = 'B'
 
 }
   
